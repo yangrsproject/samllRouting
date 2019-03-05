4f5c1ac2ddc792502d617fb242795109d187f052
yangrsproject <yangrs@aliyun.com>
2019-03-05 11:39:45 +0800

    update buildDocker.sh

--- src/main/build/shell/buildDocker.sh
+++ src/main/build/shell/buildDocker.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+DOCKER_HOME="$(cd "`dirname "$0"`"/; cd ../docker/; pwd)"
+
+JAR_HOME="$(cd "`dirname "$0"`"/; cd ../../../../target/; pwd)"
+
+cp "$JAR_HOME"/*.jar "$DOCKER_HOME"
+
 docker build -t small /home/yrs/docker/samllRouting/resource
